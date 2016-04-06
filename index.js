254722a47200c5cce1659343420d6b00fde835dd
Artem Petrov <saint.tema@gmail.com>
2016-04-06 11:32:12 -0500

    Added checks for cached templates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var utils = require("loader-utils"),
     twig = require('twig').twig;
 
 module.exports = function(content) {
-    var id = this.resource, matches, tpl;
+    var id = this.resource, matches, template, compiled;
 
     this.cacheable();
 
@@ -17,7 +17,13 @@ module.exports = function(content) {
     }
 
     id = matches.length ? matches[1] : id;
-    tpl = twig({ id: id, data: content }).compile({ module: 'node' });
 
-    return 'module.exports = ' + tpl.match(/(?:twig\()(.*)(?:\))/m)[1] + ';';
+    // Checking for cached template
+    template = twig({ ref: id });
+    if (template === null) {
+        template = twig({ id: id, data: content });
+    }
+
+    compiled = template.compile({ module: 'node' });
+    return 'module.exports = ' + compiled.match(/(?:twig\()(.*)(?:\))/m)[1] + ';';
 }
